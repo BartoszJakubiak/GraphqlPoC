4c603552116f1179f81b989d4f2c30d236442fc0
bartosz.jakubiak <bartosz.jakubiak@gmail.com>
2024-04-26 12:52:48 +0200

    first commit

---
+++ src/main/java/com/accenture/GraphqlPoC/GraphqlPoCApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.GraphqlPoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlPoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlPoCApplication.class, args);
+	}
+
+}
